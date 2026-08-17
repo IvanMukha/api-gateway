72d44ced72192e824866cb60722900a923bce72e
IvanMukha <ivan.mukha228@gmail.com>
2026-08-17 20:55:26 +0300

    create config for routing

---
+++ src/main/java/com/mukha/apigateway/config/GatewayRoutesConfig.java
@@ -0,0 +1,41 @@
+package com.mukha.apigateway.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties(GatewayRoutesConfig.GatewayServicesProperties.class)
+@RequiredArgsConstructor
+public class GatewayRoutesConfig {
+
+    private final GatewayServicesProperties properties;
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("user-service-route", r -> r
+                        .path("/v1/api/users/**", "/v1/api/cards/**")
+                        .uri(properties.userServiceUrl()))
+
+                .route("auth-service-route", r -> r
+                        .path("/v1/api/auth/**")
+                        .uri(properties.authServiceUrl()))
+
+                .route("order-service-route", r -> r
+                        .path("/v1/api/items/**", "/v1/api/orders/**")
+                        .uri(properties.orderServiceUrl()))
+                .build();
+    }
+
+    @ConfigurationProperties
+    public record GatewayServicesProperties(
+            String userServiceUrl,
+            String authServiceUrl,
+            String orderServiceUrl
+    ) {}
+}
---
+++ src/main/java/com/mukha/apigateway/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.mukha.apigateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatchers;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) {
+        return http
+                .authorizeExchange(exchanges -> exchanges
+                        .matchers(ServerWebExchangeMatchers.pathMatchers("/v1/api/auth/**")).permitAll()
+                        .anyExchange().authenticated()
+                )
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+                .build();
+    }
+}
